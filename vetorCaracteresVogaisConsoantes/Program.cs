029e78a7d6540f82b871b75d658cbd4c91e88db6
AnnyaB <anabordin2008@gmail.com>
2024-04-22 14:50:06 -0300

    ajuste

--- vetorCaracteresVogaisConsoantes/Program.cs
+++ vetorCaracteresVogaisConsoantes/Program.cs
@@ -8,8 +8,6 @@ int tamanho = 10;
 char[] palavra = new char[tamanho];
 char[] vogais = new char[5] { 'a', 'e', 'i', 'o', 'u' };
 char[] consoante = new char[21] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
-char[] alfabeto = new char[26] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-char letra;
 
 for (int i = 0; i < tamanho; i++)
 {
